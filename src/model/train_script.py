9283be78b2b6e8e6f1edf763d35c23c4709231d6
Anseur Adel <117813369+Anseuradel@users.noreply.github.com>
2025-08-04 10:59:07 +0200

    Update train_script.py

--- src/model/train_script.py
+++ src/model/train_script.py
@@ -1,7 +1,9 @@
 import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
 from trainer import ModelTrainer
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 def load_sample_data():
     """
