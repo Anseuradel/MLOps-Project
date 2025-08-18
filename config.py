b23fc94a16b21791a316a59d2575fa4594c97f8c
Anseur Adel <117813369+Anseuradel@users.noreply.github.com>
2025-08-18 16:29:21 +0200

    Update config.py

--- config.py
+++ config.py
@@ -15,3 +15,22 @@ SENTIMENT_MAPPING_3_LABEL_VERSION = {
 }
 
 LABEL_MAPPING = {1: 0, 2: 1, 3: 2, 4: 3, 5: 4}
+
+# --------------------------------------------------------------------------
+# Model config
+TOKENIZER_NAME = "bert-base-uncased"
+MODEL_NAME = "bert-base-uncased"
+
+EPOCHS = 10
+N_CLASSES = 3  # 5
+DROPOUT = 0.3
+MAX_LEN = 128
+TEST_SIZE = 0.1
+VAL_SIZE = 0.1
+BATCH_SIZE = 16
+LEARNING_RATE = 2e-5
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+# --------------------------------------------------------------------------
+# Real Dataset paths
+DATASET_PATH = "Dataset/text.csv"
