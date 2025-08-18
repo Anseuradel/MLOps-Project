8cd2fcd3f6cb1aa064e08fd15df96134e8c8d704
Anseur Adel <117813369+Anseuradel@users.noreply.github.com>
2025-08-18 13:28:57 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+# --------------------------------------------------------------------------
+# Define 5-class sentiment mapping
+SENTIMENT_MAPPING = {
+    1: "Really Negative",
+    2: "Negative",
+    3: "Neutral",
+    4: "Positive",
+    5: "Really Positive",
+}
+
+SENTIMENT_MAPPING_3_LABEL_VERSION = {
+    1: "Negative",
+    2: "Neutral",
+    3: "Positive",
+}
+
+LABEL_MAPPING = {1: 0, 2: 1, 3: 2, 4: 3, 5: 4}
