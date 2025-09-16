6542729b0388971931530f570c8a6aebb3a03def
Anseur Adel <117813369+Anseuradel@users.noreply.github.com>
2025-09-16 19:09:31 +0200

    Update main2.py

--- src/model/main2.py
+++ src/model/main2.py
@@ -9,7 +9,7 @@ from src.model.trainer import train_model
 from src.model.evaluate import evaluate
 
 def datalaoder_train_test_val(df):
-  data = create_dataloader(df, tokenizer,test_size=MAX_LEN, batch_size=BATCH_SIZE)
+  data = create_dataloader(df, tokenizer,test_size=config.MAX_LEN, batch_size=config.BATCH_SIZE)
   return data
 
 def main():
@@ -17,16 +17,16 @@ def main():
   print("Loading dataset and tokenizer\n")
 
   # Load dataset using data_extraction file's function
-  data = load_data(DATASET_PATH, merge_labels=True)
+  data = load_data(config.DATASET_PATH, merge_labels=True)
 
   # Data split before preprocessing
-  train_data_raw, test_data_raw = train_test_split(data, test_size=TEST_SIZE, random_state = 42) 
+  train_data_raw, test_data_raw = train_test_split(data, test_size=config.TEST_SIZE, random_state = 42) 
 
   print("preprocesing dataset\n")
   # Apply preprocessing using data_processing file's fucntion
-  train_data, val_data = preprocess_data(train_data_raw, test_size=VAL_SIZE, max_length= MAX_LEN)
+  train_data, val_data = preprocess_data(train_data_raw, test_size=config.VAL_SIZE, max_length=config.MAX_LEN)
 
-  tokenizer = AutoTokenizer.from_pretrained(TOKENIZER_NAME)
+  tokenizer = AutoTokenizer.from_pretrained(config.TOKENIZER_NAME)
 
   # Step 2 : Dataloader
   print("Creating dataloaders\n") 
@@ -38,11 +38,11 @@ def main():
   #Step 3 : modeling
   # Initializing model
   print("Initializing model\n")
-  model = SentimentClassifier(n_classes=N_CLASSES).to(DEVICE)
+  model = SentimentClassifier(n_classes=config.N_CLASSES).to(config.DEVICE)
 
   # Training model
   print("Training model\n")
-  trained_model = train_model(model, train_data, val_data, device=DEVICE, epochs=EPOCHS)
+  trained_model = train_model(model, train_data, val_data, device=config.DEVICE, epochs=config.EPOCHS)
 
   # Evaluate model 
   print("Evaluating model\n")
